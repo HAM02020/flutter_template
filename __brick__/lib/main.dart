97264e254650e6082b9fc98c91f72fa1a81578c4
Fansheng.mo <fansheng.mo@zkteco.com>
2023-04-28 14:31:36 +0800

    开发准备

--- __brick__/lib/main.dart
+++ __brick__/lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:{{name.snakeCase()}}/app/app.dart';
-import 'package:{{name.snakeCase()}}/app/app_bloc_oberver.dart';
-import 'package:{{name.snakeCase()}}/bloc/counter/counter_bloc.dart';
-import 'package:{{name.snakeCase()}}/bloc/user_settings/user_setting_bloc.dart';
-import 'package:{{name.snakeCase()}}/utils/log/log.dart';
+import 'package:__brick__/app/app.dart';
+import 'package:__brick__/app/app_bloc_oberver.dart';
+import 'package:__brick__/bloc/counter/counter_bloc.dart';
+import 'package:__brick__/bloc/user_settings/user_setting_bloc.dart';
+import 'package:__brick__/utils/log/log.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
