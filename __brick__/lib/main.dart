588797e74827e209830f76d55e5893e425cf3452
Fansheng.mo <fansheng.mo@zkteco.com>
2023-05-05 17:02:21 +0800

    添加App启动界面

--- __brick__/lib/main.dart
+++ __brick__/lib/main.dart
@@ -1,16 +1,23 @@
+import 'dart:async';
+
 import 'package:__brick__/app/app.dart';
 import 'package:__brick__/app/app_bloc_oberver.dart';
 import 'package:__brick__/bloc/counter/counter_bloc.dart';
 import 'package:__brick__/bloc/user_settings/user_setting_bloc.dart';
 import 'package:__brick__/utils/log/log.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  Future.delayed(const Duration(seconds: 3))
+      .then((value) => FlutterNativeSplash.remove());
   await ScreenUtil.ensureScreenSize();
 
   HydratedBloc.storage = await HydratedStorage.build(
